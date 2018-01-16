276fd0b073d975c3edb940a71274cdafa024fd3f
x1ongzhu <692949348@qq.com>
2018-01-16 23:37:38 +0800

    tencent sdk integrated

--- src/android/qqlocation.java
+++ src/android/qqlocation.java
@@ -1,11 +1,26 @@
-package cordova-plugin-qqlocation;
+package cn.x1ongzhu.qqlocation;
+
+import android.os.Looper;
+import android.support.annotation.NonNull;
+
+import com.tencent.map.geolocation.TencentLocation;
+import com.tencent.map.geolocation.TencentLocationListener;
+import com.tencent.map.geolocation.TencentLocationManager;
+import com.yanzhenjie.permission.AndPermission;
+import com.yanzhenjie.permission.Permission;
+import com.yanzhenjie.permission.PermissionListener;
+import com.yanzhenjie.permission.Rationale;
+import com.yanzhenjie.permission.RationaleListener;
 
 import org.apache.cordova.CordovaPlugin;
+
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.List;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -13,20 +28,55 @@ import org.json.JSONObject;
 public class qqlocation extends CordovaPlugin {
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        if (action.equals("getLocation")) {
+            AndPermission.with(cordova.getActivity())
+                    .requestCode(102)
+                    .permission(Permission.LOCATION)
+                    .rationale(new RationaleListener() {
+                        @Override
+                        public void showRequestPermissionRationale(int requestCode, Rationale rationale) {
+                            AndPermission.rationaleDialog(cordova.getActivity(), rationale).show();
+                        }
+                    })
+                    .callback(new PermissionListener() {
+                        @Override
+                        public void onSucceed(int requestCode, @NonNull List<String> grantPermissions) {
+                            getLocation(callbackContext);
+                        }
+
+                        @Override
+                        public void onFailed(int requestCode, @NonNull List<String> deniedPermissions) {
+                            callbackContext.error("权限不足");
+                        }
+                    })
+                    .start();
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+    private void getLocation(final CallbackContext callbackContext) {
+        TencentLocationManager locationManager = TencentLocationManager.getInstance(cordova.getActivity());
+        locationManager.requestSingleFreshLocation(new TencentLocationListener() {
+            @Override
+            public void onLocationChanged(TencentLocation tencentLocation, int i, String s) {
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("latitude", tencentLocation.getLatitude());
+                    jsonObject.put("longitude", tencentLocation.getLongitude());
+                    callbackContext.success(jsonObject);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStatusUpdate(String s, int i, String s1) {
+
+            }
+        }, Looper.getMainLooper());
     }
+
+
 }
--- www/qqlocation.js
+++ www/qqlocation.js
@@ -1,5 +1,5 @@
-var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'qqlocation', 'coolMethod', [arg0]);
+exports.getLocation = function(success, error) {
+    exec(success, error, "qqlocation", "getLocation", [{}]);
 };
